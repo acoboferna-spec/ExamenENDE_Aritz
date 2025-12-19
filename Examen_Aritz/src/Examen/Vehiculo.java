0c798a8a861eaf14142ecb12c510bec1293c2f0a
acoboferna-spec <acoboferna@educacion.navarra.es>
2025-12-19 09:32:07 +0100

    Metodos añadidos

--- Examen_Aritz/src/Examen/Vehiculo.java
+++ Examen_Aritz/src/Examen/Vehiculo.java
@@ -5,4 +5,48 @@ public class Vehiculo {
 	String marca;
 	String modelo;
 	double prestado;
+	
+	public Vehiculo(String matricula, String marca, String modelo, double prestado) {
+		this.matricula=matricula;
+		this.marca=marca;
+		this.modelo=modelo;
+		this.prestado=prestado;
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula=matricula;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca=marca;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo=modelo;
+	}
+	
+	public double getPrestado(){
+		return prestado;
+	}
+	
+	public void setPrestado(double prestado) {
+		this.prestado=prestado;
+	}
+	
+	public String toString() {
+		return "Vehiculo [matricula:" + matricula + ", marca:" + marca + ", modelo:" + modelo + ", prestado:" + prestado + "]";
+	}
 }
+
+
