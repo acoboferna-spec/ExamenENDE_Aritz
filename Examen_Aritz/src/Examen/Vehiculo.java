6739c79b284341522e284f96a0248776aebf48c0
acoboferna-spec <acoboferna@educacion.navarra.es>
2025-12-19 09:29:39 +0100

    Constructor añadido

--- Examen_Aritz/src/Examen/Vehiculo.java
+++ Examen_Aritz/src/Examen/Vehiculo.java
@@ -5,4 +5,12 @@ public class Vehiculo {
 	String marca;
 	String modelo;
 	double prestado;
+	
+	public Vehiculo(String matricula, String marca, String modelo, double prestado) {
+		this.matricula=matricula;
+		this.marca=marca;
+		this.modelo=modelo;
+		this.prestado=prestado;
+	}
 }
+
